38d8368a39880f47ccf53bae9cc0be6ccbf76f6b
Nisarg <15153@COMPND-LTP-969>
2021-09-10 18:35:00 +0530

    Add project files.

---
+++ Repo/SampleRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Repo
+{
+    public class SampleRepo
+    {
+        public IList<string> Country(string Coun)
+        {
+            return new List<string>();
+        }
+
+        public IList<string> State(string State)
+        {
+            return new List<string>();
+        }
+    }
+}
